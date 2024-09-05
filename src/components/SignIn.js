26f82ffcf93d4524236f9ffd30a90cf8519e1dea
Aman Garg <amangarg24071995@gmail.com>
2024-09-05 15:48:14 +0530

    Sign In Page Design

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const SignIn = () => {
   return (
     <div>
         <Header/>
-      <h1>hi SignIn</h1>
+        <form className='bg-black absolute p-16 w-1/3 h-auto my-36 mx-auto left-0 right-0 text-white bg-opacity-70'>
+        <h1 className='font-bold text-3xl my-3'>Sign In</h1>
+        <input className="bg-gray-950 w-full py-3 px-2 my-2 bg-opacity-85" type='text' placeholder='Email or Mobile Number'/>
+        <input className="bg-gray-950 w-full py-3 px-2 my-2 bg-opacity-85" type='password' placeholder='Password'/>
+        <button className='w-full bg-red-600 px-2 py-3 my-2'>Sign In</button>
+        <button className='my-4 px-2'>New to Netflix? Sign Up Now</button>
+        </form>
     </div>
   )
 }
