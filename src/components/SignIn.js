1483b3b2d62a0e1cff03088ac8d7d9578929a85b
Aman Garg <amangarg24071995@gmail.com>
2024-09-06 11:01:18 +0530

    Validate SignIn data and access the input through user Ref.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,21 +1,33 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Header from './Header'
+import Validate from '../utils/Validate';
 
 const SignIn = () => {
     const [isSignIn,setIsSignIn] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const mail = useRef(null);
+    const password = useRef(null);
     const toggleSignUp = () =>{
         setIsSignIn(!isSignIn);
+    }
+    const handleLogin = () =>{
+        const message = Validate(mail.current.value,password.current.value);
+        setErrorMessage(message);
+        
+
     }
   return (
     <div>
         <Header/>
-        <form className='bg-black absolute p-16 w-1/3 h-auto my-36 mx-auto left-0 right-0 text-white bg-opacity-70'>
+        <form onSubmit={(e)=>e.preventDefault()} className='bg-black absolute p-16 w-1/3 h-auto my-36 mx-auto left-0 right-0 text-white bg-opacity-70'>
         <h1 className='font-bold text-3xl my-3'>{isSignIn ? "Sign In" : "Sign Up"}</h1>
         {!isSignIn && <input className="bg-gray-950 w-full py-3 px-2 my-2 bg-opacity-85" type='text' placeholder='Full Name'/>}
-        <input className="bg-gray-950 w-full py-3 px-2 my-2 bg-opacity-85" type='text' placeholder='Email or Mobile Number'/>
-        <input className="bg-gray-950 w-full py-3 px-2 my-2 bg-opacity-85" type='password' placeholder='Password'/>
-        <button className='w-full bg-red-600 px-2 py-3 my-2'>{isSignIn ? "Sign In" : "Sign Up"}</button>
-        <p className='my-4 px-2 cursor-pointer' onClick={toggleSignUp}>{isSignIn ? "New to Netflix? Sign Up Now" : "Already registered? Sign In Now"}</p>
+        <input className="bg-gray-950 w-full py-3 px-2 my-2 bg-opacity-85" ref = {mail} type='text' placeholder='Email or Mobile Number'/>
+        <input className="bg-gray-950 w-full py-3 px-2 my-2 bg-opacity-85" ref={password} type='password' placeholder='Password'/>
+        <p className='text-red-600 text-xl'>{errorMessage}</p>
+        <button className='w-full bg-red-600 px-2 py-3 my-2' onClick={handleLogin}>{isSignIn ? "Sign In" : "Sign Up"}</button>
+        <p className='my-4 px-2 cursor-pointer' onClick={toggleSignUp}>
+            {isSignIn ? "New to Netflix? Sign Up Now" : "Already registered? Sign In Now"}</p>
         </form>
     </div>
   )
---
+++ src/utils/Validate.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Validate = (email,password) => {
+
+const isMailValid = /^[a-zA-Z0-9_.+\-]+[\x40][a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/.test(email);
+const isPasswordValid = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password);
+
+if(!isMailValid) return "Email is not Valid";
+if(!isPasswordValid) return "Password is not Valid";
+
+return null;
+  
+}
+
+export default Validate; 
